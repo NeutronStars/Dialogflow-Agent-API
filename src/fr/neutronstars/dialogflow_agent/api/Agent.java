bf4ac05312fcc25ca2844817cd201800ab1d904f
NeutronStars <pro@neutronstars.fr>
2019-05-11 19:30:40 +0200

    Add JavaDoc for the Class Agent

--- src/fr/neutronstars/dialogflow_agent/api/Agent.java
+++ src/fr/neutronstars/dialogflow_agent/api/Agent.java
@@ -4,19 +4,71 @@ import java.util.Collection;
 import java.util.Optional;
 
 /**
- * interface Agent.
+ * The core of Dialogflow Agent. You can access the agent content from this instance.
  */
-public interface Agent{
+public interface Agent
+{
 
-  String getName();
+    /**
+     * Get the name of your agent.
+     *
+     * @return agent name.
+     */
+    String getName();
 
-  Collection<? extends Intent> getItents();
+    /**
+     * Get all intents of your agent.
+     * {@link Intent}
+     *
+     * @return the list of intents.
+     */
+    Collection<? extends Intent> getIntents();
 
-  Optional<? extends Intent> getItentByName(String name);
+    /**
+     * Get intent with the name.
+     * <br>If the intent <b>does not exist</b> then the optional <b>will be empty</b>.
+     *
+     * <h2>Example</h2>
+     * <pre><code>
+     *     agent.getIntentByName("MyIntentName").ifPresent(intent {@link Intent} -> {
+     *        System.out.println(intent.getName());
+     *     });
+     * </code></pre>
+     *
+     * @param name The name of the intent.
+     * @return optional of the intent.
+     */
+    Optional<? extends Intent> getIntentByName(String name);
 
-  Collection<? extends Entity> getEntities();
+    /**
+     * Get all entities of your agent.
+     * {@link Entity}
+     *
+     * @return the list of entities.
+     */
+    Collection<? extends Entity> getEntities();
 
-  Optional<? extends Entity> getEntityByName(String name);
+    /**
+     * Get entity with the name.
+     * <br>If the entity <b>does not exist</b> then the optional <b>will be empty</b>.
+     *
+     * <h2>Example</h2>
+     * <pre><code>
+     *     agent.getEntityByName("MyEntityName").ifPresent(entity {@link Entity} -> {
+     *        System.out.println(entity.getName());
+     *     });
+     * </code></pre>
+     *
+     * @param name The name of the entity.
+     * @return optional of the entity.
+     */
+    Optional<? extends Entity> getEntityByName(String name);
 
-  Settings getSettings();
+    /**
+     * Get settings of your agent.
+     * {@link Settings}
+     *
+     * @return settings.
+     */
+    Settings getSettings();
 }
